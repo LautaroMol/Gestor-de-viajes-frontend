d17717abeecedd5b68d8da2bafb1a1205f44e5d8
Lautaro Castellanos <laumol159@gmail.com>
2024-05-07 20:01:06 -0300

    config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViajesComponent } from './Componentes/viajes/viajes.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, ViajesComponent]
 })
 export class AppComponent {
   title = 'Camiones';
+  
 }
