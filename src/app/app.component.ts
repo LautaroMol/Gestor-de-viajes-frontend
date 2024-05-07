4f392ee7098ad5c9464d90c81e08e31d32a9911b
Lautaro Castellanos <laumol159@gmail.com>
2024-05-07 20:58:14 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ViajesComponent } from './Componentes/viajes/viajes.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ViajesComponent]
+    imports: [RouterOutlet, ViajesComponent,MatButtonModule, MatTooltipModule, MatIconModule]
 })
 export class AppComponent {
+
   title = 'Camiones';
   
 }
