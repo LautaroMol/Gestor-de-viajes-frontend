3434ec936adfd7d0b38abc8b88d7043f373ef0d3
Lautaro Castellanos <laumol159@gmail.com>
2024-05-08 20:56:34 -0300

    set

--- src/app/Componentes/viajes/viajes.component.ts
+++ src/app/Componentes/viajes/viajes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-viajes',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './viajes.component.html',
   styleUrl: './viajes.component.css'
 })
