cbef13c01abd2fb5538667d6628b6f60f1b53459
lucascostamagna19@outlook.com <lucascostamagna19@outlook.com>
2024-06-27 21:45:36 -0300

    s

--- src/app/Componentes/gastos/gastos.component.ts
+++ src/app/Componentes/gastos/gastos.component.ts
@@ -8,77 +8,77 @@ import { GastosFormComponent } from '../../Modals/gastos-form/gastos-form.compon
 import { DeleteGastoComponent } from '../../Modals/gastos-delete/gastos-delete.component';
 
 @Component({
-  selector: 'app-gastos',
-  standalone: true,
-  imports: [CommonModule, MatDialogModule],
-  providers: [GastoService, HttpClient],
-  templateUrl: './gastos.component.html',
-  styleUrls: ['./gastos.component.css'],
+	selector: 'app-gastos',
+	standalone: true,
+	imports: [CommonModule, MatDialogModule],
+	providers: [GastoService, HttpClient],
+	templateUrl: './gastos.component.html',
+	styleUrls: ['./gastos.component.css'],
 })
 export class GastosComponent implements OnInit {
-  gastos: Gasto[] = [];
-  mostrarFormulario: boolean = false;
-  modoEdicion: boolean = false;
+	gastos: Gasto[] = [];
+	mostrarFormulario: boolean = false;
+	modoEdicion: boolean = false;
 
-  constructor(private gastoService: GastoService, private dialog: MatDialog) { }
+	constructor(private gastoService: GastoService, private dialog: MatDialog) { }
 
-  ngOnInit(): void {
-    this.obtenerGastos();
-  }
+	ngOnInit(): void {
+		this.obtenerGastos();
+	}
 
-  obtenerGastos() {
-    this.gastoService.getList().subscribe({
-      next: (data) => {
-        this.gastos = data;
-        console.log(this.gastos);
-      },
-      error: (e) => {
-        console.error(e);
-      },
-    });
-  }
+ 	obtenerGastos() {
+		this.gastoService.getList().subscribe({
+			next: (data) => {
+				this.gastos = data;
+				console.log(this.gastos);
+			},
+			error: (e) => {
+				console.error(e);
+			},
+		});
+ 	}
 
-  editarGasto(gasto: Gasto) {
-    const dialogRef = this.dialog.open(GastosFormComponent, {
-      data: gasto
-    });
+	editarGasto(gasto: Gasto) {
+		const dialogRef = this.dialog.open(GastosFormComponent, {
+			data: gasto
+		});
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === 'Editado') {
-        this.obtenerGastos();
-      }
-    });
-  }
+		dialogRef.afterClosed().subscribe(result => {
+			if (result === 'Editado') {
+				this.obtenerGastos();
+			}
+		});
+	}
 
-  borrarGasto(gasto: Gasto) {
-    const dialogRef = this.dialog.open(DeleteGastoComponent, {
-      disableClose: true,
-      width: "400px",
-      data: gasto
-    });
+	borrarGasto(gasto: Gasto) {
+		const dialogRef = this.dialog.open(DeleteGastoComponent, {
+			disableClose: true,
+			width: "400px",
+			data: gasto
+		});
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === "Eliminar") {
-        this.gastoService.delete(gasto).subscribe({
-          next: (data) => {
-            console.log("Gasto borrado exitosamente");
-            this.obtenerGastos();
-          },
-          error: (e) => {
-            console.error("No se ha podido borrar el gasto", e);
-          }
-        });
-      }
-    });
-  }
+		dialogRef.afterClosed().subscribe(result => {
+			if (result === "Eliminar") {
+				this.gastoService.delete(gasto).subscribe({
+					next: (data) => {
+						console.log("Gasto borrado exitosamente");
+						this.obtenerGastos();
+					},
+					error: (e) => {
+						console.error("No se ha podido borrar el gasto", e);
+					}
+				});
+			}
+		});
+	}
 
-  nuevoGasto() {
-    const dialogRef = this.dialog.open(GastosFormComponent);
+	nuevoGasto() {
+		const dialogRef = this.dialog.open(GastosFormComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === 'Creado') {
-        this.obtenerGastos();
-      }
-    });
-  }
+		dialogRef.afterClosed().subscribe(result => {
+			if (result === 'Creado') {
+				this.obtenerGastos();
+			}
+		});
+	}
 }
