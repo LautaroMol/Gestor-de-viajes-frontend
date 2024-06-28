016d65fd89250fc699a99990fb0c0474940deac3
lucascostamagna19@outlook.com <lucascostamagna19@outlook.com>
2024-06-27 23:36:59 -0300

    resubida

--- src/app/Modals/categoria-delete/categoria-delete.component.ts
+++ src/app/Modals/categoria-delete/categoria-delete.component.ts
@@ -5,24 +5,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-categoria-delete',
-  standalone: true,
-  imports: [MatButtonModule, MatDialogModule],
-  templateUrl: './categoria-delete.component.html',
-  styleUrls: ['./categoria-delete.component.css']
+    selector: 'app-categoria-delete',
+    standalone: true,
+    imports: [MatButtonModule, MatDialogModule],
+    templateUrl: './categoria-delete.component.html',
+    styleUrls: ['./categoria-delete.component.css']
 })
 export class CategoriaDeleteComponent implements OnInit {
 
-  constructor(
-    private dialogoReferencia: MatDialogRef<CategoriaDeleteComponent>,
-    @Inject(MAT_DIALOG_DATA) public dataCategoria: Categoria
-  ) {}
+ 	constructor(
+		private dialogoReferencia: MatDialogRef<CategoriaDeleteComponent>,
+		@Inject(MAT_DIALOG_DATA) public dataCategoria: Categoria
+  	) {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
 
-  confirmacion() {
-    if (this.dataCategoria) {
-      this.dialogoReferencia.close("Eliminar");
+    confirmacion() {
+        if (this.dataCategoria) {
+            this.dialogoReferencia.close("Eliminar");
+        }
     }
-  }
 }
