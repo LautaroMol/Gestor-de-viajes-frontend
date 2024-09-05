c3001be6e4a78274318324de6d4697b4dba11c32
Lautaro Castellanos <laumol159@gmail.com>
2024-09-05 12:16:29 -0300

    s

--- src/app/Componentes/camion/camion.component.ts
+++ src/app/Componentes/camion/camion.component.ts
@@ -13,20 +13,18 @@ import { UnidadService } from '../../Services/unidad.service';
 export class CamionComponent implements OnInit {
   unidad: Unidad | null = null;
   selectedWheel: number | null = null;
-  oilBarrelColor: string = '#e8eaed'; 
-  oilAnimation: string = '';           
   showWarning: boolean = false;
 
   constructor(private unidadService: UnidadService) {}
 
   ngOnInit(): void {
-    this.getCamion(1); // Se busca la unidad por id
+    this.getCamion(1); // Obtener la unidad por id
   }
 
   getCamion(id: number): void {
     this.unidadService.get(id).subscribe(data => {
       this.unidad = data;
-      this.updateOilBarrel(); // Llama a la función después de que los datos se cargan
+      this.checkOilWarning(); // Verificar si es necesario el cambio de aceite
     });
   }
 
@@ -41,31 +39,18 @@ export class CamionComponent implements OnInit {
 
   getOilLevel(): number {
     const maxKm = 50000;
-    if (this.unidad) {
-      // Calcular el porcentaje de llenado en base a los kilómetros recorridos
-      const kmAceite = this.unidad.kmAceite;
-      const oilLevel = Math.max(0, (maxKm - kmAceite) / maxKm);
-      return oilLevel * 110; // La altura del barril es 110, entonces usamos eso como referencia
+    let oilLevel = 0;
+    if (this.unidad != null) {
+      oilLevel = Math.max(0, (maxKm - this.unidad.kmAceite) / maxKm);
     }
-    return 0;
+    return oilLevel * 100; // Porcentaje de llenado
   }
 
-  updateOilBarrel() {
-    if (this.unidad) {
-      const kmAceite = this.unidad.kmAceite;
-      const oilLevel = 1 - (kmAceite / 50000);
-
-      // Modificar el color del barril según el nivel
-      this.oilBarrelColor = `rgba(0, 0, 255, ${oilLevel})`;  // Cuanto mayor el kilometraje, más vacío el barril (menos azul)
-
-      // Mostrar advertencia si está cerca de 50,000 km
-      if (kmAceite >= 45000) {
-        this.showWarning = true;
-        this.oilAnimation = 'blink 1s infinite';  // Activar parpadeo
-      } else {
-        this.showWarning = false;
-        this.oilAnimation = '';  // Desactivar parpadeo
-      }
+  checkOilWarning() {
+    if (this.unidad && this.unidad.kmAceite >= 45000) {
+      this.showWarning = true;
+    } else {
+      this.showWarning = false;
     }
   }
 }
