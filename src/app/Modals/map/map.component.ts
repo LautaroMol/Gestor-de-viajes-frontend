0e9d6733f883f9dc3d2703183bc95da53d198fc3
Lautaro Castellanos <laumol159@gmail.com>
2024-09-05 08:54:15 -0300

    map

--- src/app/Modals/map/map.component.ts
+++ src/app/Modals/map/map.component.ts
@@ -1,232 +1,150 @@
-import {Component,OnInit,ViewChild,ElementRef,AfterViewInit,OnDestroy,} from '@angular/core';
+import { Component, OnInit, AfterViewInit, Output, EventEmitter } from '@angular/core';
+import L from 'leaflet';
+import "leaflet-routing-machine";
 import { PlacesService } from '../../Services/place.service';
-import { CommonModule } from '@angular/common';
-import { Map, MapStyle, config,Marker, MaptilerNavigationControl, MaptilerGeolocateControl } from '@maptiler/sdk';
-import '@maptiler/sdk/dist/maptiler-sdk.css';
-import { GeocodingControl } from "@maptiler/geocoding-control/maptilersdk";
-import "@maptiler/sdk/dist/maptiler-sdk.css";
-import "@maptiler/geocoding-control/style.css";
-
+import Geocoder from 'leaflet-control-geocoder';
 
 @Component({
-  selector: 'app-map',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css'],
+	selector: 'app-map',
+	standalone: true,
+	imports: [],
+	templateUrl: './map.component.html',
+	styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
-  geo: any;
-  map: Map | undefined;
-  currentLocationMarker: Marker | undefined;
-  @ViewChild('map')
-  private mapContainer!: ElementRef<HTMLElement>;
+export class MapComponent implements OnInit, AfterViewInit {
+	geo: any;
+	map: any;
+	currentLocationMarker: any;
+	chosenLocationMarker: any;
+	isLocated = false;
+	routeControl: any;
+	
+	@Output() locationSelected = new EventEmitter<[number, number]>();
+	@Output() destinationSelect = new EventEmitter<[number, number]>();
+	@Output() distanceCalculated = new EventEmitter<number>();
 
-  constructor(private placeSvc: PlacesService) {}
-  ngOnDestroy(): void {
-    this.map?.remove();
-  }
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          async (position) => {
-            
-            const lat = position.coords.latitude;
-            const lng = position.coords.longitude;
-  
-            console.log('con alta precisión lat: ', lat, ' long: ', lng);
-  
-            const initialState = {
-              lng: lng,
-              lat: lat,
-              zoom: 14
-            };
-  
-            this.map = new Map({
-              container: this.mapContainer.nativeElement,
-              style: MapStyle.STREETS,
-              center: [initialState.lng, initialState.lat],
-              zoom: initialState.zoom,
-              terrainControl: true
-            });
-            const gc = new GeocodingControl();
-            this.map.addControl(gc);
-            this.map.addControl(new MaptilerGeolocateControl({
-              positionOptions:{
-                enableHighAccuracy:true
-              },
-              trackUserLocation: true,
-              showAccuracyCircle: true,
-              showUserLocation: true
-            }));
-          },
-          (error) => {
-            console.error('Error al obtener la geolocalización', error);
-          },
-          {
-            enableHighAccuracy: true, // Alta precisión
-            timeout: 5000, // 5 segundos de tiempo máximo
-            maximumAge: 0 // No usar una posición en caché
-          }
-        );
-      } else {
-        console.error('La geolocalización no es compatible con este navegador');
-      }
-    }, 2000);
-  }
-  
-  ngOnInit(): void {
-    config.apiKey = 'oEDh6mPK2TIhdFrpa70J';
-  }
+	constructor(private placeSvc: PlacesService) {}
 
-  locate(): void {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const lat = position.coords.latitude;
-          const lng = position.coords.longitude;
+	Reload() {
+		localStorage.removeItem('geoLoc');
+		location.reload();
+	}
 
-          if (this.map) {
-            // Mover el mapa a la ubicación actual
-            this.map.flyTo({center: [lng, lat],zoom: 14});
+	Locate() {
+		if (this.map && this.geo && !this.isLocated) {
+			this.geo = this.placeSvc.userLocation;
 
-            // Si hay un amrcador lo actualizamos
-            if (this.currentLocationMarker) {
-              this.currentLocationMarker.setLngLat([lng, lat]);
-            } else {
-              
-              this.currentLocationMarker = new Marker()
-                .setLngLat([lng, lat])
-                .addTo(this.map);
-            }
-          }
-        },
-        (error) => {
-          console.error('Error al obtener la geolocalización', error);
-        },
-        {
-          enableHighAccuracy: true, // Alta precisión
-          timeout: 5000, // 5 segundos de tiempo máximo
-          maximumAge: 0 // No usar una posición en caché
-        }
-      );
-    } else {
-      console.error('La geolocalización no es compatible con este navegador');
-    }
-  }
-  //    @Inject(PLATFORM_ID) private platformId: Object
-  // Reload() {
-  //   localStorage.removeItem('geoLoc');
-  //   location.reload();
-  // }
+			this.currentLocationMarker = L.marker(this.geo)
+				.addTo(this.map)
+				.bindPopup('<b>Ubicación actual</b>')
+				.openPopup();
 
-  // Locate() {
-  //   if (this.map && this.geo && !this.isLocated) {
-  //     this.geo = this.placeSvc.userLocation;
+			this.isLocated = true;
+			localStorage.setItem('geoLoc', JSON.stringify(this.geo));
 
-  //     this.currentLocationMarker = L.marker(this.geo)
-  //       .addTo(this.map)
-  //       .bindPopup('<b>Ubicación actual</b>')
-  //       .openPopup();
+			this.currentLocationMarker.on('moveend', () => {
+				const latLng = this.currentLocationMarker.getLatLng();
+				this.geo = [latLng.lat, latLng.lng];
+				localStorage.setItem('geoLoc', JSON.stringify(this.geo));
+			});
 
-  //     this.isLocated = true;
+			this.locationSelected.emit(this.geo);
+			this.map.flyTo(this.geo, 13);
+		}
+	}
 
-  //     localStorage.setItem('geoLoc', JSON.stringify(this.geo));
+	CalculateRoute() {
+		if (this.map && this.currentLocationMarker && this.chosenLocationMarker) {
+			const start = this.currentLocationMarker.getLatLng();
+			const end = this.chosenLocationMarker.getLatLng();
 
-  //     this.currentLocationMarker.on('moveend', () => {
-  //       const latLng = this.currentLocationMarker.getLatLng();
-  //       this.geo = [latLng.lat, latLng.lng];
-  //       localStorage.setItem('geoLoc', JSON.stringify(this.geo));
-  //     });
+			if (this.routeControl) {
+				this.map.removeControl(this.routeControl);
+			}
 
-  //     this.locationSelected.emit(this.geo);
-  //     this.map.flyTo(this.geo, 13);
-  //   }
-  // }
+			this.routeControl = L.Routing.control({
+				waypoints: [
+					L.latLng(start.lat, start.lng),
+					L.latLng(end.lat, end.lng)
+				],
+				routeWhileDragging: true,
+				show: false
+			}).addTo(this.map);
 
-  // CalculateRoute() {
-  //   if (this.map && this.currentLocationMarker && this.chosenLocationMarker) {
-  //     const start = this.currentLocationMarker.getLatLng();
-  //     const end = this.chosenLocationMarker.getLatLng();
+			const distanceInMeters = this.map.distance(start, end);
+			const distanceInKilometers = distanceInMeters / 1000;
+			alert(`La distancia entre los marcadores es de ${distanceInKilometers.toFixed(2)} kilómetros.`);
+		}
+	}
 
-  //     if (this.routeControl) {
-  //       this.map.removeControl(this.routeControl);
-  //     }
+	ngOnInit() {
+		this.isLocated = false;
+		if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+			document.addEventListener('userLocationReady', () => {
+				this.initializeMap();
+				setTimeout(() => {
+					this.geo = this.placeSvc.userLocation;
+					if (this.geo) {
+						localStorage.setItem('geoLoc', JSON.stringify(this.geo));
+					}
+				}, 2000);
+			});
+		}
+	}
 
-  //     this.routeControl = L.Routing.control({
-  //       waypoints: [
-  //         L.latLng(start.lat, start.lng),
-  //         L.latLng(end.lat, end.lng),
-  //       ],
-  //       routeWhileDragging: true,
-  //       show: false,
-  //     }).addTo(this.map);
+	ngAfterViewInit() {
+		if (this.placeSvc.userLocation) {
+			setTimeout(() => {
+				this.geo = this.placeSvc.userLocation;
+				if (this.geo) {
+					this.initializeMap();
+				}
+			}, 3000);
+		}
+	}
 
-  //     const distanceInKilometers = this.map.distance(start, end) / 1000;
-  //     this.distanceCalculated.emit(distanceInKilometers);
-  //   }
-  // }
+	initializeMap() {
+		if (this.map) {
+			return;
+		}
 
-  // ngAfterViewInit() {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     if (this.placeSvc.userLocation) {
-  //       setTimeout(() => {
-  //         this.geo = this.placeSvc.userLocation;
-  //         if (this.geo) {
-  //           this.initializeMap();
-  //         }
-  //       }, 2000);
-  //     }
-  //   }
-  // }
+		this.geo = this.placeSvc.userLocation;
+		if (this.geo) {
+			this.map = L.map('map').setView(this.geo, 13);
 
-  // initializeMap() {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     if (this.map) {
-  //       return;
-  //     }
+			L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				maxZoom: 19,
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+			}).addTo(this.map);
+      
+			// Geocoder integration
+			const geocoder = new Geocoder({
+				defaultMarkGeocode: false,
+			})
+			.on('markgeocode', (e: any) => {
+				const bbox = e.geocode.bbox;
+				const poly = L.polygon([
+					bbox.getSouthEast(),
+					bbox.getNorthEast(),
+					bbox.getNorthWest(),
+					bbox.getSouthWest(),
+				]).addTo(this.map);
+				this.map.fitBounds(poly.getBounds());
+			})
+			.addTo(this.map);
 
-  //     this.geo = this.placeSvc.userLocation;
-  //     if (this.geo) {
-  //       this.map = L.map('map').setView(this.geo, 13);
-
-  //       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  //         maxZoom: 19,
-  //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  //       }).addTo(this.map);
-
-  //       // Corrección del error: Uso correcto de L.Control.Geocoder
-  //       this.placeSvc.L.Control.geocoder({
-  //         defaultMarkGeocode: false
-  //       })
-  //         .on('markgeocode', (e: any) => {
-  //           const bbox = e.geocode.bbox;
-  //           const poly = L.polygon([
-  //             bbox.getSouthEast(),
-  //             bbox.getNorthEast(),
-  //             bbox.getNorthWest(),
-  //             bbox.getSouthWest()
-  //           ]).addTo(this.map);
-  //           this.map.fitBounds(poly.getBounds());
-  //         })
-  //         .addTo(this.map);
-
-  //       this.map.on('click', (e: L.LeafletMouseEvent) => {
-  //         const { lat, lng } = e.latlng;
-
-  //         if (this.chosenLocationMarker) {
-  //           this.chosenLocationMarker.setLatLng([lat, lng]);
-  //         } else {
-  //           this.chosenLocationMarker = L.marker([lat, lng], { draggable: true })
-  //             .addTo(this.map)
-  //             .bindPopup('<b>Ubicación destino</b>')
-  //             .openPopup();
-  //         }
-
-  //         this.destinationSelect.emit([lat, lng]);
-  //       });
-  //     }
-  //   }
-  // }
+			this.map.on('click', (e: L.LeafletMouseEvent) => {
+				const { lat, lng } = e.latlng;
+				if (this.chosenLocationMarker) {
+					this.chosenLocationMarker.setLatLng([lat, lng]);
+				} else {
+					this.chosenLocationMarker = L.marker([lat, lng], { draggable: true })
+						.addTo(this.map)
+						.bindPopup('<b>Ubicación elegida</b>')
+						.openPopup();
+				}
+				this.destinationSelect.emit([lat, lng]);
+			});
+		}
+	}
 }
--- src/app/Services/place.service.ts
+++ src/app/Services/place.service.ts
@@ -1,38 +1,37 @@
-import { Inject, Injectable,PLATFORM_ID } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import 'leaflet-routing-machine';
 
 @Injectable({
-  	providedIn: 'root'
+	providedIn: 'root'
 })
-@Injectable()
 export class PlacesService {
-
-	public L:any = null;
-	public Routing:any = null;
 	public userLocation?: [number, number];
 
-
-	constructor(){
-		this.getUserLocation();
+	constructor(@Inject(PLATFORM_ID) private platformId: object) {
+		if (isPlatformBrowser(platformId)) {
+			this.initUserLocation();
+		}
 	}
 
-	// private initUserLocation() {
-	// 	if (typeof window !== 'undefined' && navigator.geolocation) {
-	// 		this.getUserLocation();
-	// 	} else {
-	// 		this.userLocation = undefined;
-	// 	}
-	// }
+	private initUserLocation() {
+		if (navigator.geolocation) {
+			this.getUserLocation();
+		} else {
+			this.userLocation = undefined;
+		}
+	}
 
-	 private getUserLocation() {
-	 	navigator.geolocation.getCurrentPosition(
-	 		({ coords }) => {
-	 			this.userLocation = [coords.latitude, coords.longitude];
-	 			document.dispatchEvent(new Event('userLocationReady'));
-	 		}, (error) => {
-	 			console.error('Error getting location:', error);
-	 			this.userLocation = undefined;
-	 		}
-	 	);
-	 }
+	private getUserLocation() {
+		navigator.geolocation.getCurrentPosition(
+			({ coords }) => {
+				this.userLocation = [coords.latitude, coords.longitude];
+				document.dispatchEvent(new Event('userLocationReady'));
+			},
+			(error) => {
+				console.error('Error getting location:', error);
+				this.userLocation = undefined;
+			}
+		);
+	}
 }
