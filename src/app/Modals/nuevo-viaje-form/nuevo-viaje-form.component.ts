7d370707d5439bdd559313ba19f6f31c59221d39
Lautaro Castellanos <laumol159@gmail.com>
2024-09-16 10:01:16 -0300

    test

--- src/app/Modals/nuevo-viaje-form/nuevo-viaje-form.component.ts
+++ src/app/Modals/nuevo-viaje-form/nuevo-viaje-form.component.ts
@@ -35,7 +35,7 @@ export class NuevoViajeFormComponent implements OnInit {
             inicio: ['', Validators.required],
             final: ['', Validators.required],
             distancia: ['', Validators.required],
-            gastos: [null],
+            gastos: [0],
             fecha: ['', Validators.required],
             cp: [0,Validators.required],
             facturado: [false],
