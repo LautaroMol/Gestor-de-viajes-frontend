ea1dbb6e24bac7bbf904d930af919bf75d5edaa6
Lautaro Castellanos <laumol159@gmail.com>
2024-09-27 13:16:26 -0300

    s

--- src/app/Componentes/viajes/viajes.component.ts
+++ src/app/Componentes/viajes/viajes.component.ts
@@ -112,24 +112,43 @@ export class ViajesComponent implements OnInit {
   }
   celebrated: boolean = false;
   celebrate() {
-    if (this.celebrated) return; 
+    const duration = 15 * 1000;
+    const animationEnd = Date.now() + duration;
+    let skew = 1;
+  
 
-    const duration = 3000;
-    this.audioCelebration.currentTime = 0;
+    function randomInRange(min: number, max: number) {
+      return Math.random() * (max - min) + min;
+    }
+  
+    (function frame() {
+      const timeLeft = animationEnd - Date.now();
+      const ticks = Math.max(200, 500 * (timeLeft / duration));
+      skew = Math.max(0.8, skew - 0.001);
+  
+      confetti({
+        particleCount: 1,
+        startVelocity: 0,
+        ticks: ticks,
+        origin: {
+          x: Math.random(),
+          y: (Math.random() * skew) - 0.2 
+        },
+        colors: ['#ffffff'],
+        shapes: ['circle'],
+        gravity: randomInRange(0.4, 0.6),
+        scalar: randomInRange(0.4, 1),
+        drift: randomInRange(-0.4, 0.4)
+      });
+  
+      if (timeLeft > 0) {
+        requestAnimationFrame(frame);
+      }
+    }());
+  
+    this.audioCelebration.currentTime = 0; 
     this.audioCelebration.play();
-
-    confetti({
-        particleCount: 100,
-        spread: 160,
-        origin: { y: 0.6 },
-        colors: ['#FF0000', '#FFFFFF'],
-        gravity: -0.3,
-    });
-
-    setTimeout(() => {
-        this.celebrated = true;
-        confetti.reset();
-    }, duration);
-}
+  }
+  
 
 }
