a9b326558280dd4cbcd8cf705ab78b1d62ed9e08
Lautaro Castellanos <laumol159@gmail.com>
2024-09-27 12:53:50 -0300

    s

--- src/app/Componentes/viajes/viajes.component.ts
+++ src/app/Componentes/viajes/viajes.component.ts
@@ -10,7 +10,7 @@ import { MatDialog } from '@angular/material/dialog';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { AmortizacionService } from '../../Services/amortizacion.service';
 import { Amortizacion } from '../../Interfaces/amortizacion';
-import * as confetti from 'canvas-confetti';
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-viajes',
@@ -112,7 +112,7 @@ export class ViajesComponent implements OnInit {
     const duration = 3000; // in milliseconds
   
     confetti({
-      particleCount: 100,
+      particleCount: 1000,
       spread: 160,
       origin: { y: 0.6 },
     });
