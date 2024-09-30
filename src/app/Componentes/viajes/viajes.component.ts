13a3b4fd40a85955510a92b7da802d8bbace7b99
Lautaro Castellanos <laumol159@gmail.com>
2024-09-30 13:57:09 -0300

    test

--- src/app/Componentes/viajes/viajes.component.ts
+++ src/app/Componentes/viajes/viajes.component.ts
@@ -4,43 +4,56 @@ import { TotalsCardsComponent } from '../totals-cards/totals-cards.component';
 import { Viaje } from '../../Interfaces/viaje';
 import { ViajeService } from '../../Services/viaje.service';
 import { CommonModule } from '@angular/common';
-import { MapComponent } from "../../Modals/map/map.component";
+import { MapComponent } from '../../Modals/map/map.component';
 import { NuevoViajeFormComponent } from '../../Modals/nuevo-viaje-form/nuevo-viaje-form.component';
 import { MatDialog } from '@angular/material/dialog';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { AmortizacionService } from '../../Services/amortizacion.service';
 import { Amortizacion } from '../../Interfaces/amortizacion';
 import confetti from 'canvas-confetti';
+import pdfMake from 'pdfmake/build/pdfMake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'app-viajes',
   standalone: true,
-  imports: [MatCardModule, TotalsCardsComponent, CommonModule, MapComponent,MatProgressBarModule],
+  imports: [
+    MatCardModule,
+    TotalsCardsComponent,
+    CommonModule,
+    MapComponent,
+    MatProgressBarModule,
+  ],
   templateUrl: './viajes.component.html',
-  styleUrls: ['./viajes.component.css'] 
+  styleUrls: ['./viajes.component.css'],
 })
 export class ViajesComponent implements OnInit {
-
   viajes: Viaje[] = [];
   amortizacion?: Amortizacion;
-  progreso: number = 0; 
+  progreso: number = 0;
   audioCelebration: HTMLAudioElement;
+  pdfjsLib: any;
 
-  constructor(private viajeService: ViajeService, private dialog: MatDialog,
-    private amortizacionService: AmortizacionService,
+  constructor(
+    private viajeService: ViajeService,
+    private dialog: MatDialog,
+    private amortizacionService: AmortizacionService
   ) {
     this.audioCelebration = new Audio('assets/audio/yippie.mp3');
-   }
+  }
 
   ngOnInit(): void {
     this.obtenerViajes();
     this.obtenerAmortizacion();
+    this.pdfjsLib = this.pdfjsLib
   }
 
   obtenerViajes() {
     this.viajeService.getList().subscribe({
       next: (data) => {
-        this.viajes = data.filter(viaje => !viaje.borrado);
+        this.viajes = data.filter((viaje) => !viaje.borrado);
         console.log(this.viajes);
       },
       error: (e) => {
@@ -49,19 +62,20 @@ export class ViajesComponent implements OnInit {
       },
     });
   }
-  
+
   calcularSubtotal(gastos: (number | null)[] | null): number {
-    if (!gastos || gastos.every(g => g === null)) {
+    if (!gastos || gastos.every((g) => g === null)) {
       return 0;
     }
-    return gastos.filter((g): g is number => g !== null)
-                 .reduce((total, gasto) => total + gasto, 0);
+    return gastos
+      .filter((g): g is number => g !== null)
+      .reduce((total, gasto) => total + gasto, 0);
   }
-  
+
   BorrarViajes(viaje: Viaje): void {
     this.viajeService.delete(viaje.idViaje).subscribe({
       next: () => {
-        this.viajes = this.viajes.filter(v => v.idViaje !== viaje.idViaje);
+        this.viajes = this.viajes.filter((v) => v.idViaje !== viaje.idViaje);
       },
       error: (e) => {
         console.error(e);
@@ -72,64 +86,79 @@ export class ViajesComponent implements OnInit {
 
   editarViaje(viaje: Viaje) {
     const dialogRef = this.dialog.open(NuevoViajeFormComponent, {
-      data: viaje
+      data: viaje,
     });
-    dialogRef.afterClosed().subscribe(result => {
-        this.obtenerViajes()
+    dialogRef.afterClosed().subscribe((result) => {
+      this.obtenerViajes();
     });
   }
 
   verCartaPorte(cp: string): void {
     this.viajeService.getCartaPorte(cp).subscribe({
-        next: (blob: Blob) => {
-            const url = window.URL.createObjectURL(blob); // Crear una URL para el blob
-            const newTab = window.open(url); // Abrir la URL en una nueva pestaña
-            if (!newTab) {
-                console.error('No se pudo abrir la nueva pestaña. Verifique que no esté bloqueada.');
-            }
-        },
-        error: (err) => {
-            console.error('Error al obtener la Carta de Porte', err);
+      next: (blob: Blob) => {
+        const url = window.URL.createObjectURL(blob); // Crear una URL para el blob
+        const newTab = window.open(url); // Abrir la URL en una nueva pestaña
+        if (!newTab) {
+          console.error(
+            'No se pudo abrir la nueva pestaña. Verifique que no esté bloqueada.'
+          );
         }
+      },
+      error: (err) => {
+        console.error('Error al obtener la Carta de Porte', err);
+      },
     });
   }
 
-  obtenerAmortizacion(){
-    this.amortizacionService.get(1).subscribe(data => {
+  obtenerAmortizacion() {
+    this.amortizacionService.get(1).subscribe((data) => {
       this.amortizacion = data;
-    })
+    });
   }
 
   calcularPorcentajeAmortizacion(): number {
     if (this.amortizacion) {
-      const restanteAnual = this.amortizacion.objetivo - this.amortizacion.recaudado;
+      const restanteAnual =
+        this.amortizacion.objetivo - this.amortizacion.recaudado;
       if (restanteAnual <= 0) {
         return 100;
       }
-      return (this.amortizacion.recaudado / this.amortizacion.objetivo) * 100.;
+      return (this.amortizacion.recaudado / this.amortizacion.objetivo) * 100;
     }
     return 0;
   }
   celebrated: boolean = false;
   celebrate() {
-    if (this.celebrated) return; 
+    if (this.celebrated) return;
 
-    const duration =5000;
+    const duration = 5000;
     this.audioCelebration.currentTime = 0;
     this.audioCelebration.play();
 
     confetti({
-        particleCount: 500,
-        spread: 130,
-        origin: { y: 0.6 },
-        colors: ['#FF0000', '#FFFFFF'],
-        gravity: 0.6,
+      particleCount: 500,
+      spread: 130,
+      origin: { y: 0.6 },
+      colors: ['#FF0000', '#FFFFFF'],
+      gravity: 0.6,
     });
 
     setTimeout(() => {
-        this.celebrated = true;
-        confetti.reset();
+      this.celebrated = true;
+      confetti.reset();
     }, duration);
-}
+  }
+  GeneratePDF() {
+    const pdfDefinition: any={
+      content:[
+        {
+          text: "hola mundo"
+        }
+      ]
+    }
+  
+  const pdf = pdfMake.createPDF(pdfDefinition);
+  pdf.open();
+  }
 
 }
