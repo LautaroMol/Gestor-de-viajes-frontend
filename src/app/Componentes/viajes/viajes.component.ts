ce6dcd4bdf82ba066364192cbeac34fa161c9a59
Lautaro Castellanos <laumol159@gmail.com>
2024-09-30 22:21:26 -0300

    filt

--- src/app/Componentes/viajes/viajes.component.ts
+++ src/app/Componentes/viajes/viajes.component.ts
@@ -229,7 +229,7 @@ export class ViajesComponent implements OnInit {
         const userCompany = this.user ? this.user.domicilio : 'Compañía del Cliente';
         const userCuit = this.user ? this.user.cuit : 'CUIT del Cliente';
         const userCondition = this.user ? this.user.condicion : 'Condición del Cliente';
-        this.gastos.filter(gasto => gasto.idGasto == viaje.gastos).forEach(gasto => viaje.gastos);
+        this.gastos.includes(viaje.gastos);
         //encontrar al cliente
         const cliente = this.clientes.find(cliente => cliente.cuitCliente == viaje.cuitUsuario);
         const clienteNombre = cliente ? cliente.razonSoc : "No se encontró el cliente ni su CUIT en la base de datos, por favor cárguelo y verifique que no se haya borrado.";
@@ -501,6 +501,7 @@ export class ViajesComponent implements OnInit {
         // Crear y abrir el PDF
         const pdf = pdfMake.createPdf(dd);
         pdf.open();
+        this.obtenerGastos();
     }).catch(err => {
         console.error('Error al cargar la imagen:', err);
     });
