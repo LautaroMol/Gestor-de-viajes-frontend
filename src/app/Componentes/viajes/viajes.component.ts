a2f0acc28e4f75d6c886c571bb10cb6a50e6b3cc
Lautaro Castellanos <laumol159@gmail.com>
2024-09-30 22:44:41 -0300

    s

--- src/app/Componentes/viajes/viajes.component.ts
+++ src/app/Componentes/viajes/viajes.component.ts
@@ -233,7 +233,7 @@ export class ViajesComponent implements OnInit {
         const userCompany = this.user ? this.user.domicilio : 'Compañía del Cliente';
         const userCuit = this.user ? this.user.cuit : 'CUIT del Cliente';
         const userCondition = this.user ? this.user.condicion : 'Condición del Cliente';
-        this.gastos.includes(viaje.gastos);
+
         //encontrar al cliente
         const cliente = this.clientes.find(cliente => cliente.cuitCliente == viaje.cuitUsuario);
         const clienteNombre = cliente ? cliente.razonSoc : "No se encontró el cliente ni su CUIT en la base de datos, por favor cárguelo y verifique que no se haya borrado.";
@@ -242,7 +242,7 @@ export class ViajesComponent implements OnInit {
         // Usar los datos del viaje seleccionado
         const viajeFecha = new Date(viaje.fecha).toLocaleDateString();
         const viajeNumeroRecibo = viaje.idViaje;
-        const sumaGastos = this.gastos.reduce((total, gasto) => total + gasto.cantidad,0);
+
 
         const dd = {
             content: [
