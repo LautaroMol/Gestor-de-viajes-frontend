febc235854aa40ce00b31eb1d5be7a8491e796f8
Lautaro Castellanos <laumol159@gmail.com>
2024-10-28 08:42:40 -0300

    node

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient(withFetch()),
-    importProvidersFrom(HttpClientModule)
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient(withFetch())
   ]
 };
