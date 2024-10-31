871ab9b302ddb49f48174b90691748782a88bd39
Lautaro Castellanos <laumol159@gmail.com>
2024-10-31 20:42:22 -0300

    register rep

--- src/app/Componentes/registro/registro.component.ts
+++ src/app/Componentes/registro/registro.component.ts
@@ -5,53 +5,59 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { Login } from '../../Interfaces/login';
 import { Router } from '@angular/router';
 import { User } from '../../Interfaces/user';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,ReactiveFormsModule],
+  imports: [MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,ReactiveFormsModule,CommonModule],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
   private accesoService = inject(AccesoService);
-
   private router = inject(Router);
   public formBuild = inject(FormBuilder);
 
   public formRegistro: FormGroup = this.formBuild.group({
-    nombre: ['',Validators.required],
-    correo:['',Validators.required],
-    clave: ['',Validators.required]
-  })
+    nombre: ['', Validators.required],
+    correo: ['', [Validators.required, Validators.email]],
+    clave: ['', Validators.required]
+  });
 
-  registrarse(){
-    if(this.formRegistro.invalid) return;
+  public errorMessage: string = ''; // Propiedad para almacenar el mensaje de error
+
+  registrarse() {
+    if (this.formRegistro.invalid) return;
 
     const obj: User = {
       nombre: this.formRegistro.value.nombre,
       correo: this.formRegistro.value.correo,
       pass: this.formRegistro.value.clave
-    }
-    this.accesoService.register(obj).subscribe ({
-      next:(data)=> {
-        if(data.isSuccess){
+    };
+
+    this.accesoService.register(obj).subscribe({
+      next: (data) => {
+        if (data.isSuccess) {
           this.router.navigate(['/viajes']);
-        }else{
-          alert("no se pudo registrar")
+        } else {
+          this.errorMessage = "No se pudo registrar. Intente nuevamente.";
         }
-      }, error:(error)=>{
-        console.log(error.message);
+      },
+      error: (error) => {
+        if (error.status === 409) { // 409 Conflict: Correo ya registrado
+          this.errorMessage = error.error.message; // Asigna el mensaje de error del backend
+        } else {
+          this.errorMessage = "Ocurrió un error inesperado. Intente nuevamente.";
+        }
       }
-    })
+    });
   }
 
-  volver(){
+  volver() {
     this.router.navigate(['']);
   }
-
 }
