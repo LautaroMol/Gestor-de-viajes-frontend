5ffb33937ee57e906db6f67464247f114e4f39bb
Lautaro Castellanos <laumol159@gmail.com>
2024-11-04 00:17:00 -0300

    a

--- src/app/Componentes/viajes/viajes.component.ts
+++ src/app/Componentes/viajes/viajes.component.ts
@@ -20,8 +20,6 @@ import { ClienteService } from '../../Services/cliente.service';
 import { ViajeEventService } from '../../Services/viaje-event.service';
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
-const pdfMake = require('pdfmake/build/pdfmake');
-const pdfFonts = require('pdfmake/build/vfs_fonts');
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 
